83a71ad06a8fc7e20f48a07ba793d057f8c6a8aa
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2023-09-06 18:01:10 -0300

    folders and first component added

---
+++ src/componentes/Comentario.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Comentario() {
+  return (
+    <div className="contenedor-comentarios">
+      <img className="imagen-comentario" 
+      src={require("../imgs/img-comentario-carlos.png")} alt="Foto de Carlos">
+      </img>
+      <div className="contenedor-text-comentario">
+        <p className="nombre-pais-comentario">Carlos Duty en Iran</p>
+        <p className="cargo-comentario">Ingeniero en pastas</p>
+        <p className="texto-comentario">"Fue la mejor de las pastas, fue la peor de las pastas." </p>
+      </div>
+    </div>
+  );
+}
+
+export default Comentario;
